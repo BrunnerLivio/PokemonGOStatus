9bd9c3fc1882df31a05abf8fbd1480348a2e12e0
Livio Brunner <livio.brunner@hotmail.com>
2016-07-22 20:23:10 +0200

    configure acces-control-allow-origin

--- server/src/server.js
+++ server/src/server.js
@@ -9,6 +9,17 @@ var mongoose = require('mongoose');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  var allowedOrigins = ['http://brunnerliv.io', 'http://localhost:8080'];
+  var origin = req.headers.origin;
+  if(allowedOrigins.indexOf(origin) > -1){
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  return next();
+});
 var port = 8888;
 
 var router = express.Router();
