993ff98f2197433f3c3ba20afef6b7555622fe6a
Livio Brunner <livio.brunner@hotmail.com>
2016-07-22 19:26:56 +0200

    remove babel

--- server/src/isPokemonGoUp.js
+++ server/src/isPokemonGoUp.js
@@ -1,3 +1,4 @@
+'use strict';
 
 var fetch = require('node-fetch');
 var timeout = require('promise.timeout');
@@ -5,16 +6,22 @@ var time = require('promise-time');
 
 var url = 'https://pgorelease.nianticlabs.com/plfe/';
 
-function getfetchPromise () {
-  const timePromise = time(fetch)(url);
-  return timePromise.then(() => timePromise.time).catch(() => -1);
+function getfetchPromise() {
+  var timePromise = time(fetch)(url);
+  return timePromise.then(function () {
+    return timePromise.time;
+  }).catch(function () {
+    return -1;
+  });
 }
 
-function getTimePromise () {
-  return timeout(getfetchPromise, 3500, true)().catch(() => -1);
+function getTimePromise() {
+  return timeout(getfetchPromise, 3500, true)().catch(function () {
+    return -1;
+  });
 }
 
-function isPokemonGoUp () {
+function isPokemonGoUp() {
   return getTimePromise();
 }
 module.exports = isPokemonGoUp;
